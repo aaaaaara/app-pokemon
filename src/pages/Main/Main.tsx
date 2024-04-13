74b829d3e66cfe1e8fcc479489e584b7d3cb0e31
aaaaaara <araring.j@gmail.com>
2024-04-13 23:02:06 +0900

    feat: 파일 생성

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header/Header';
+
+function Main() {
+  return (
+    <section>
+      <Header />
+      <p>여기 검색창</p>
+      <p>여기는 카드 리스트</p>
+    </section>
+  );
+}
+
+export default Main;
